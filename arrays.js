e5fb701b0053dc00b1fb96dcc92fcb70a404a6df
Aastha <saxena.aastha91@gmail.com>
2022-08-29 13:00:24 -0400

    Added two arrays to arrays.js

--- arrays.js
+++ arrays.js
@@ -19,6 +19,34 @@ let beverages = [
     { id: 5, name: "Water", qty_available: 11, tagline: "It won't get you drunk", first_brewed: "unknown", description: "This delicious beverage is all natural!" }
   ]
 
-  let arrays = [characters, beverages]
+  let marvelCharacters = [
+    { id: 1, name: 'Captain America', year_created: 1941, power_source: 'Injected with Super-Soldier serum', weapons: ['Vibranium shield']},
+    { id: 2, name: 'Black Panther', year_created: 1966, power_source: 'Having the juices of the Heart-Shaped Herb applied to his body', weapons: ['Energy dagger', 'anti-metal claws']},
+    { id: 3, name: 'Groot', year_created: 1960, power_source: 'Born as a Flora colossus', weapons: ['His powers']},
+    { id: 4, name: 'Hawkeye', year_created: 1964, power_source: 'Trained his skills in marksmanship', weapons: ['Bows and arrows']},
+    { id: 5, name: 'Black Widow', year_created: 1964, power_source: 'Trained as a KGB operative and dangerous assassin', weapons: ['Batons', 'taser disks', 'handguns']},
+    { id: 6, name: 'The Winter Soldier', year_created: 1941, power_source: 'Injected with the infinity formula', weapons: ['Snipers', 'rifles']},
+    { id: 7, name: 'Star-Lord', year_created: 1976, power_source: 'Part human and part celestial', weapons: ['Element Gun']},
+    { id: 8, name: 'Iron Man', year_created: 1963, power_source: 'His suit', weapons: ['Bows and arrowRepulsor rays', 'uni-beam projector', 'lasers']},
+    { id: 9, name: 'Hulk', year_created: 1962, power_source: 'exposed to gamma radiation', weapons: ['His strength']},
+    { id: 10, name: 'Spider man', year_created: 1963, power_source: 'Bitten by a radioactive spider', weapons: ['Web-shooters']},
+    { id: 11, name: 'Captain Marvel', year_created: 1968, power_source: 'Exposed to Kree technology', weapons: ['Nega Bands']},
+    { id: 12, name: 'Doctor Strange', year_created: 1963, power_source: 'Trained by Ancient One', weapons: ['Cloak of Levitations', 'Eye of Agamotto', 'Orb of Agamotto']},
+    { id: 13, name: 'Thor', year_created: 1962, power_source: 'Son of Asgardian God of Thunder', weapons: ['Warhammer Mjolnir']},
+    { id: 14, name: 'Antman', year_created: 1962, power_source: 'Tested the Pym Particles serum on himself', weapons: ['Stinger firearm']}
+  ]
+
+  let richestPeople = [
+    {id: 1, name: "Elon Musk", net_worth: '$219,000,000,000', age: 50, country: 'USA', source: 'Tesla, SpaceX', industry: 'Automotive'},
+    {id: 2, name: "Jeff Bezos", net_worth: '$171,000,000,000', age: 58, country: 'USA', source: 'Amazon', industry: 'Technology'},
+    {id: 3, name: "Benard Arnault", net_worth: '$158,000,000,000', age: 73, country: 'France', source: 'LVMH', industry: 'Fashion and Retail'},
+    {id: 4, name: "Bill Gates", net_worth: '$129,000,000,000', age: 66, country: 'USA', source: 'Microsoft', industry: 'Technology'},
+    {id: 5, name: "Warren Buffett", net_worth: '$118,000,000,000', age: 91, country: 'USA', source: 'Berkshire Hathway', industry: 'Finance & Investments'},
+    {id: 6, name: "Larry Page", net_worth: '$111,000,000,000', age: 49, country: 'USA', source: 'Google', industry: 'Technology'},
+    {id: 7, name: "sergey Brin", net_worth: '$107,000,000,000', age: 48, country: 'USA', source: 'Google', industry: 'Technology'},
+
+  ]
+
+  let arrays = [characters, beverages, marvelCharacters, richestPeople]
 
  
