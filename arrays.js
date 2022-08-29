d27fc1aca158763e54a7cf0694cab4b9c203d9e8
Aastha <saxena.aastha91@gmail.com>
2022-08-29 17:11:53 -0400

    Added more arrays to arrays.js

--- arrays.js
+++ arrays.js
@@ -20,33 +20,75 @@ let beverages = [
   ]
 
   let marvelCharacters = [
-    { id: 1, name: 'Captain America', year_created: 1941, power_source: 'Injected with Super-Soldier serum', weapons: ['Vibranium shield']},
-    { id: 2, name: 'Black Panther', year_created: 1966, power_source: 'Having the juices of the Heart-Shaped Herb applied to his body', weapons: ['Energy dagger', 'anti-metal claws']},
-    { id: 3, name: 'Groot', year_created: 1960, power_source: 'Born as a Flora colossus', weapons: ['His powers']},
-    { id: 4, name: 'Hawkeye', year_created: 1964, power_source: 'Trained his skills in marksmanship', weapons: ['Bows and arrows']},
-    { id: 5, name: 'Black Widow', year_created: 1964, power_source: 'Trained as a KGB operative and dangerous assassin', weapons: ['Batons', 'taser disks', 'handguns']},
-    { id: 6, name: 'The Winter Soldier', year_created: 1941, power_source: 'Injected with the infinity formula', weapons: ['Snipers', 'rifles']},
-    { id: 7, name: 'Star-Lord', year_created: 1976, power_source: 'Part human and part celestial', weapons: ['Element Gun']},
-    { id: 8, name: 'Iron Man', year_created: 1963, power_source: 'His suit', weapons: ['Bows and arrowRepulsor rays', 'uni-beam projector', 'lasers']},
-    { id: 9, name: 'Hulk', year_created: 1962, power_source: 'exposed to gamma radiation', weapons: ['His strength']},
-    { id: 10, name: 'Spider man', year_created: 1963, power_source: 'Bitten by a radioactive spider', weapons: ['Web-shooters']},
-    { id: 11, name: 'Captain Marvel', year_created: 1968, power_source: 'Exposed to Kree technology', weapons: ['Nega Bands']},
-    { id: 12, name: 'Doctor Strange', year_created: 1963, power_source: 'Trained by Ancient One', weapons: ['Cloak of Levitations', 'Eye of Agamotto', 'Orb of Agamotto']},
-    { id: 13, name: 'Thor', year_created: 1962, power_source: 'Son of Asgardian God of Thunder', weapons: ['Warhammer Mjolnir']},
-    { id: 14, name: 'Antman', year_created: 1962, power_source: 'Tested the Pym Particles serum on himself', weapons: ['Stinger firearm']}
+    { id: 1, name: 'Captain America', year_created: 1941, power_source: 'Injected with Super-Soldier serum', weapons: ['Vibranium shield'] },
+    { id: 2, name: 'Black Panther', year_created: 1966, power_source: 'Having the juices of the Heart-Shaped Herb applied to his body', weapons: ['Energy dagger', 'anti-metal claws'] },
+    { id: 3, name: 'Groot', year_created: 1960, power_source: 'Born as a Flora colossus', weapons: ['His powers'] },
+    { id: 4, name: 'Hawkeye', year_created: 1964, power_source: 'Trained his skills in marksmanship', weapons: ['Bows and arrows'] },
+    { id: 5, name: 'Black Widow', year_created: 1964, power_source: 'Trained as a KGB operative and dangerous assassin', weapons: ['Batons', 'taser disks', 'handguns'] },
+    { id: 6, name: 'The Winter Soldier', year_created: 1941, power_source: 'Injected with the infinity formula', weapons: ['Snipers', 'rifles'] },
+    { id: 7, name: 'Star-Lord', year_created: 1976, power_source: 'Part human and part celestial', weapons: ['Element Gun'] },
+    { id: 8, name: 'Iron Man', year_created: 1963, power_source: 'His suit', weapons: ['Bows and arrowRepulsor rays', 'uni-beam projector', 'lasers'] },
+    { id: 9, name: 'Hulk', year_created: 1962, power_source: 'exposed to gamma radiation', weapons: ['His strength'] },
+    { id: 10, name: 'Spider man', year_created: 1963, power_source: 'Bitten by a radioactive spider', weapons: ['Web-shooters'] },
+    { id: 11, name: 'Captain Marvel', year_created: 1968, power_source: 'Exposed to Kree technology', weapons: ['Nega Bands'] },
+    { id: 12, name: 'Doctor Strange', year_created: 1963, power_source: 'Trained by Ancient One', weapons: ['Cloak of Levitations', 'Eye of Agamotto', 'Orb of Agamotto'] },
+    { id: 13, name: 'Thor', year_created: 1962, power_source: 'Son of Asgardian God of Thunder', weapons: ['Warhammer Mjolnir'] },
+    { id: 14, name: 'Antman', year_created: 1962, power_source: 'Tested the Pym Particles serum on himself', weapons: ['Stinger firearm'] }
   ]
 
   let richestPeople = [
-    {id: 1, name: "Elon Musk", net_worth: '$219,000,000,000', age: 50, country: 'USA', source: 'Tesla, SpaceX', industry: 'Automotive'},
-    {id: 2, name: "Jeff Bezos", net_worth: '$171,000,000,000', age: 58, country: 'USA', source: 'Amazon', industry: 'Technology'},
-    {id: 3, name: "Benard Arnault", net_worth: '$158,000,000,000', age: 73, country: 'France', source: 'LVMH', industry: 'Fashion and Retail'},
-    {id: 4, name: "Bill Gates", net_worth: '$129,000,000,000', age: 66, country: 'USA', source: 'Microsoft', industry: 'Technology'},
-    {id: 5, name: "Warren Buffett", net_worth: '$118,000,000,000', age: 91, country: 'USA', source: 'Berkshire Hathway', industry: 'Finance & Investments'},
-    {id: 6, name: "Larry Page", net_worth: '$111,000,000,000', age: 49, country: 'USA', source: 'Google', industry: 'Technology'},
-    {id: 7, name: "sergey Brin", net_worth: '$107,000,000,000', age: 48, country: 'USA', source: 'Google', industry: 'Technology'},
-
+    { id: 1, name: "Elon Musk", net_worth: '$219,000,000,000', age: 50, country: 'USA', source: 'Tesla, SpaceX', industry: 'Automotive' },
+    { id: 2, name: "Jeff Bezos", net_worth: '$171,000,000,000', age: 58, country: 'USA', source: 'Amazon', industry: 'Technology' },
+    { id: 3, name: "Benard Arnault", net_worth: '$158,000,000,000', age: 73, country: 'France', source: 'LVMH', industry: 'Fashion and Retail' },
+    { id: 4, name: "Bill Gates", net_worth: '$129,000,000,000', age: 66, country: 'USA', source: 'Microsoft', industry: 'Technology' },
+    { id: 5, name: "Warren Buffett", net_worth: '$118,000,000,000', age: 91, country: 'USA', source: 'Berkshire Hathway', industry: 'Finance & Investments' },
+    { id: 6, name: "Larry Page", net_worth: '$111,000,000,000', age: 49, country: 'USA', source: 'Google', industry: 'Technology' },
+    { id: 7, name: "sergey Brin", net_worth: '$107,000,000,000', age: 48, country: 'USA', source: 'Google', industry: 'Technology' },
   ]
 
-  let arrays = [characters, beverages, marvelCharacters, richestPeople]
+  let harryPotterBooks = [
+    { id: 1, title: "Harry Potter and the Sorcerer's Stone", release_date_US: '9 January, 1998' },
+    { id: 2, title: "Harry Potter and the Chamber of Secrets", release_date_US: '9 February, 1999' },
+    { id: 3, title: "Harry Potter and the Prisioner of Azkaban", release_date_US: '9 August, 1999' },
+    { id: 4, title: "Harry Potter and the Goblet of Fire", release_date_US: '8 July, 2000' },
+    { id: 5, title: "Harry Potter and the Order of Phoenix", release_date_US: '21 June, 2003' },
+    { id: 6, title: "Harry Potter and the Half-Blood Prince", release_date_US: '16 July, 2005' },
+    { id: 7, title: "Harry Potter and the Deathly Hallows", release_date_US: '21 July, 2007' },
+    { id: 8, title: "Harry Potter and the Cursed Child", release_date_US: '31 July, 2016' }
+  ]
+  
+  let burgers = [
+    { id: 1, name: "50/50 burger", origin: 'California, USA', description: "Half ground bacon, half ground beef burger patty developed by Scott Slater for Slater's 50/50 restaurant." },
+    { id: 2, name: "Angus burger", origin: 'Scotland', description: "A hamburger made using beef from Angus cattle." },
+    { id: 3, name: "Aussie Burger/Kiwiburger", origin: 'Australia', description: "Popular regional hamburger ingredients in Australia and New Zealand include canned beetroot, pineapple and a fried egg." },
+    { id: 4, name: "Bacon cheeseburger", origin: 'USA', description: "Hamburger with bacon and cheese is a bacon cheeseburger, which became an official menu item at an A&W Restaurant owned by Dale Mulder in Lansing, Michigan in 1963." },
+    { id: 5, name: "Banquet burger", origin: 'Toronto, Canada', description: "Similar to a bacon cheeseburger, this burger is often served open-faced, with the hamburger patty, cheese, and bacon on a flat bottom bun next to the top rounded bun containing the lettuce, tomato, and onion slices." },
+    { id: 6, name: "Barbeque burger", origin: 'Texas, USA', description: "Prepared with ground beef, mixed with onions and barbecue sauce, and then grilled." },
+    { id: 7, name: "Buffalo burger", origin: 'New York, USA', description: "Prepared with meat from the American Bison,[11] buffalo burgers have less cholesterol, less fat, and fewer calories than beef hamburgers and chicken hamburgers. " },
+    { id: 8, name: "Cheeseburger", origin: 'USA', description: "The cheese is usually sliced, then added a short time before the hamburger finishes cooking to allow it to melt. In fast food restaurants, the cheese that is added to a cheeseburger is typically American cheese, but there are many other variations." },
+  ]
+
+  rainbowColors = [
+    { id: 1, name: "red", hex_code: '#FF0000', decimal: '255,0,0' },
+    { id: 2, name: "orange", hex_code: '#FFA500', decimal: '255,165,0' },
+    { id: 3, name: "yellow", hex_code: '#FFFF00', decimal: '255,255,0' },
+    { id: 4, name: "green", hex_code: '008000', decimal: '0,128,0' },
+    { id: 5, name: "blue", hex_code: '#0000FF', decimal: '0,0,255' },
+    { id: 6, name: "indigo", hex_code: '#4B0082', decimal: '75,0,130' },
+    { id: 7, name: "violet", hex_code: '#EE82EE', decimal: '238,130,238' }
+  ]
+
+  wondersOfWorld = [
+    { id: 1, name: 'Taj mahal', place: 'India' },
+    { id: 2, name: 'Christ the Redeemer, Rio de Janeiro', place: 'Rio de Janerio, Brazil' },
+    { id: 3, name: 'Grat Wall of China', place: 'Beijing, China' },
+    { id: 4, name: 'Petra, Jordan', place: 'Jordan' },
+    { id: 5, name: 'Machu Picchu', place:'Peru' },
+    { id: 6, name: 'Chichen Itza', place: 'Mexico' },
+    { id: 7, name: 'Colosseum', place: 'Italy' }
+  ]
+
+
+  let arrays = [characters, beverages, marvelCharacters, richestPeople, harryPotterBooks, burgers, rainbowColors, wondersOfWorld]
 
  
